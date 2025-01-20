e60023f855b0bf010a24cbedfe201f84ae846889
SamriddhDiwan <samriddhdiwan7@gmail.com>
2025-01-20 17:01:50 +0530

    exercise card and searchble menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
-import React from "react";
-
-function App(){
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import ExerciseCard from "./Exercisecard";
+import NewExercise from "./Searchable";
+import exerciseList from "./Exercise";
+function App() {
+    const [currExercises, handleExercises] = useState({ "Chest Press": [ {sno: "1", reps: "10", weight: "10"} ] });
+    const [value, setValue] = useState("");
+    function addNewSet(newSetDetails, exerciseName) {
+        handleExercises((prev) => {
+            return {
+                ...prev,
+                [exerciseName]: [
+                    ...(prev[exerciseName]),
+                    {
+                        sno: prev[exerciseName].length+ 1,
+                        reps: newSetDetails.reps,
+                        weight: newSetDetails.weight
+                    }
+                ]
+            };
+        }
+        )
+    };
+    function addNewExerciseCard(exerciseName) {
+        console.log(exerciseName);
+        handleExercises({...currExercises,[exerciseName]:[]});
+    }
     return (
         <div>
-            <h1>Hello from the app </h1>
+
+
+
+
+
+            <NewExercise
+                options={exerciseList}
+                label="name"
+                id="id"
+                selectedVal={value}
+                handleChange={(val) => setValue(val)}
+                addNewExercise={addNewExerciseCard}
+            />
+
+            {Object.keys(currExercises).map((key) => {
+                return (<ExerciseCard
+                    exerciseName={key}
+                    addNewSet={addNewSet}
+                    sets={currExercises[key]}
+                />);
+            })}
+
+
+
+
         </div>
     );
 }
---
+++ src/Searchable.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./Searchable.module.css";
+
+
+function NewExercise(props){
+  const [query,setQuery]=useState("");
+  const [isOpen,setIsOpen]=useState(false);
+  const inputRef=useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (inputRef.current && !inputRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
+  function getDisplayValue(){
+    if(query) return query;
+    return props.selectedVal;
+  }
+  function toggle(event){
+    setIsOpen(event&&event.target===inputRef.current);
+  }
+  function filter(options){
+    return options.filter(option=>
+      option[props.label].toLowerCase().includes(query.toLowerCase())
+    )
+  }
+  function selectOption(option){
+    console.log(option.label)
+    props.addNewExercise(option[props.label]);
+    setQuery('');
+    props.handleChange(option[props.label]);
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div className={styles.dropdown}>
+      <div className={styles.control}>
+        <div className={styles.selectedValue}>
+        <input
+        placeholder="Select exercise"
+          ref={inputRef}
+          value={getDisplayValue()}
+          type="text"
+          name="searchTerm"
+          onChange={(event)=>{
+            setQuery(event.target.value);
+            props.handleChange(null);
+          }}
+          onClick={toggle}
+        />
+        </div>
+      </div>
+      <div className={`${styles.arrow} ${isOpen ? styles.open : ""}`} />
+      <div className={`${styles.options} ${isOpen ? styles.open : ""}`}>
+        {
+          filter(props.options).map((option,index)=>{
+              return (
+                <div onClick={()=>{
+                  selectOption(option)
+                }}
+                className={`${styles.option} ${option[props.label]===props.selectedVal?styles.selected:""}`}
+                key={`${props.id}-${index}`}
+                >
+                  {option[props.label]}
+                </div>
+              )
+          })
+        }
+      </div>
+      
+    </div>
+  );
+}
+export default NewExercise;
