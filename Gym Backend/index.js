181641df63eff587d8861096476b0eee050dee49
SamriddhDiwan <samriddhdiwan7@gmail.com>
2025-01-26 02:11:50 +0530

    color as exercise gets added

--- Gym Backend/index.js
+++ Gym Backend/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import exerciseData from './exerciseData.js';
-
+import cors from 'cors';
 const app=express();
 const port=process.env.port||5000;
 
-
+app.use(cors())
 app.get('/',(req,res)=>{
     res.json(exerciseData)
 })
