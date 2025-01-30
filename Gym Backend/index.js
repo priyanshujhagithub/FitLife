596a7078add2fd3645365525b8c90311061f0472
SamriddhDiwan <samriddhdiwan7@gmail.com>
2025-01-30 15:31:00 +0530

    new Changes

--- Gym Backend/index.js
+++ Gym Backend/index.js
@@ -4,11 +4,14 @@ import cors from 'cors';
 const app=express();
 const port=process.env.port||5000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.json(exerciseData)
 })
-
+app.post('/addNewExercise',(req,res)=>{
+    console.log(req.body);
+})
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
--- Gym Frontend/src/Components/CurrentWorkout/CurrentWorkoutMain.jsx
+++ Gym Frontend/src/Components/CurrentWorkout/CurrentWorkoutMain.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-
+import axios from 'axios';
 function CurrentWorkoutMain() {
-    const [currExercises, handleExercises] = useState({ "Chest Press": [ {sno: "1", reps: "10", weight: "10"} ] });
+    const [currExercises, handleExercises] = useState({ "Chest Press": [{ sno: "1", reps: "10", weight: "10" }] });
     const [value, setValue] = useState("");
-    const [selectedMuscleGroup,setSelectedMuscleGroup]=useState([]);
+    const [selectedMuscleGroup, setSelectedMuscleGroup] = useState([]);
+    function endAndPostWorkoutSession() {
+        const userData={
+            data:currExercises
+        }
+        async function makePostRequest(){
+            await axios.post("http://localhost:5000/addNewExercise",userData);
+        }
+        makePostRequest();
+    }
     function addNewSet(newSetDetails, exerciseName) {
         handleExercises((prev) => {
             return {
@@ -12,7 +21,7 @@ function CurrentWorkoutMain() {
                 [exerciseName]: [
                     ...(prev[exerciseName]),
                     {
-                        sno: prev[exerciseName].length+ 1,
+                        sno: prev[exerciseName].length + 1,
                         reps: newSetDetails.reps,
                         weight: newSetDetails.weight
                     }
@@ -22,17 +31,17 @@ function CurrentWorkoutMain() {
         )
     };
     function addNewExerciseCard(exerciseName) {
-        setSelectedMuscleGroup([...selectedMuscleGroup,...exerciseName.muscleGrp]);
+        setSelectedMuscleGroup([...selectedMuscleGroup, ...exerciseName.muscleGrp]);
         console.log(selectedMuscleGroup);
-        handleExercises({...currExercises,[exerciseName.name]:[]});
+        handleExercises({ ...currExercises, [exerciseName.name]: [] });
     }
     return (
         <div>
 
-        <Outlet
-            context={{value:value,setValue:setValue,addNewExerciseCard:addNewExerciseCard,addNewSet:addNewSet,currExercises:currExercises,selectedMuscleGroup:selectedMuscleGroup}}
+            <Outlet
+                context={{ value: value, setValue: setValue, addNewExerciseCard: addNewExerciseCard, addNewSet: addNewSet, currExercises: currExercises, selectedMuscleGroup: selectedMuscleGroup, endAndPostWorkoutSession: endAndPostWorkoutSession }}
 
-        />
+            />
         </div>
     );
 }
