5f13df7eaa48055136a4b84a4c2cd80234f5e7a0
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-05-19 00:52:51 +0530

    exerciseRoute created

---
+++ Gym Backend/routes/exerciseRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { addExercise,getMyExercises } from '../controllers/exerciseController';
+import {protect} from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.post('/add',protect,addExercise);
+router.get('/my',protect,getMyExercises);
+
+export default router;
