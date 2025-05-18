1866f45670678f0318114a5d59e05d846165723d
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-05-19 00:34:15 +0530

    Middleware added for verifying token and to Authorize

---
+++ Gym Backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel';
+
+export const protect= async(req,res,next)=>{
+    try{
+        let token=null;
+        if(req.cookies && req.cookies.token){
+            token=req.cookies.token;
+        }
+        
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message:'Not authorized, token missing'
+            });
+        }
+
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        const user= await User.findById(decoded.id).select('-password');
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:'Not authorized,user not found'
+            });
+        }
+        req.user=user;
+        next();
+    }catch(err){
+        return res.status(401).json({
+            success:false,
+            message:'Not authorized, token invalid'
+        });
+    }
+};
