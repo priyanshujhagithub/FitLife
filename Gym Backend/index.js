d4941f4bf358775bcdbc0fb6b623e7d0dafd6e0c
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-05-19 01:15:16 +0530

    backend index.js updated with new end points

--- Gym Backend/index.js
+++ Gym Backend/index.js
@@ -4,7 +4,9 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
-import authRouter from  './routes/authRoutes.js'
+import authRouter from  './routes/authRoutes.js';
+import exerciseRouter from './routes/exerciseRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app=express();
 const port=process.env.port||3001;
@@ -23,10 +25,13 @@ app.get('/',(req,res)=>{
 });
 //API Endpoints
 app.use('/auth', authRouter);
+app.use('/api/exercises', exerciseRouter);
+app.use('/api/users', userRouter);  
 
-app.post('/addNewExercise',(req,res)=>{
-    console.log(req.body.data);
-});
+//Yeh wala hta diya hai iske jagah pe exerciserouter use ho rha hai
+// app.post('/addNewExercise',(req,res)=>{
+//     console.log(req.body.data);
+// });
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
