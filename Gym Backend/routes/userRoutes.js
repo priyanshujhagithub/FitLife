fded4b64eb749aeaa67ba028acce31f0810419e5
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-05-19 00:53:22 +0530

    userRoute created

---
+++ Gym Backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getUserProfile } from '../controllers/userController';
+import {protect} from '../middleware/authMiddleware.js';
+
+const router= express.Router();
+router.get('/profile',protect,getUserProfile);
+
+export default router;
