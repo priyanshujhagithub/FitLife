b47a938638b6fc2d5c92e1bd2978424d88cc995b
Priyanshu Jha <129042608+priyanshujhagithub@users.noreply.github.com>
2025-09-10 00:41:42 +0530

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL ='https://fitlife-wygr.onrender.com' || 'https://localhost:3001';
 
-const API_BASE_URL ='http://localhost:3001';
+const API_BASE_URL ='https://fitlife-wygr.onrender.com';
 
 export const API_ENDPOINTS = {
   AUTH: {
